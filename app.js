82e2760522178e4d5ac66988d847727b5b380821
unknown <harshit.sri2211@gmail.com>
2023-04-29 19:41:58 +0530

    res header change

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(cors({
  }))
  app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Expose-Headers","*")
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
  });
