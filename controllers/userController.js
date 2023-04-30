e3e9ad6ab16959db9bccd65c6fe48e63258f3b58
unknown <harshit.sri2211@gmail.com>
2023-04-30 12:08:16 +0530

    same site none

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,7 +54,7 @@ exports.loginUser = catchAsyncError(async(req,res)=>{
     res.cookie('chatapp',cookieToken,{
         secure: true,
         httpOnly: true,
-        sameSite:'strict'
+        sameSite:'none'
     })
     return res.status(200).send({
         status:'success',
